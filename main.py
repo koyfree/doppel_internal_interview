22d63075996e390aec0d3848915c6bda3212ff69
koyfree <koyfree@gmail.com>
2025-07-31 16:24:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if st.session_state.phase == "intro":
 
     # 2. 닉네임 입력
     if user_id.strip().isdigit() and len(user_id.strip()) == 5:
-        nickname = st.text_input("챗봇이 당신을 뭐라고 부르면 좋을까요? 예: 슬기님, 준호, 별명 등", key="nickname_input")
+        nickname = st.text_input("챗봇이 당신을 뭐라고 부르면 좋을까요? 예: 카리나님, 윈터님 등. 이 별명은 저장되지 않습니다 :)", key="nickname_input")
 
         if nickname.strip():
             st.markdown(f"""
@@ -37,7 +37,7 @@ if st.session_state.phase == "intro":
                 st.session_state.messages_dislikes = []
                 st.session_state.messages_weekly = []
                 st.session_state.messages = []
-                st.experimental_rerun()
+                st.rerun()
     else:
         st.warning("고유 ID는 숫자 5자리여야 해요.")
 
