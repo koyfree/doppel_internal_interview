ff2021f888f4ca7879796bf55252a646c49d3d58
koyfree <koyfree@gmail.com>
2025-08-08 11:39:36 +0900

    Update interview.py

--- interview.py
+++ interview.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 import time
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 import gspread
 
 LIKE_END = "Thank you for sharing all these details."
